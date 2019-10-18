341992e679fa694b096af56ae0576759cf9dd573
Kedar Bhanegaonkar <ksbhanegaonkar@gmail.com>
2019-10-18 18:51:35 +0530

    Added customer console logic.. need to fix some null pointer issues

--- src/myreactapp/src/components/CustomerConsole/CustomerConsole.js
+++ src/myreactapp/src/components/CustomerConsole/CustomerConsole.js
@@ -1,39 +1,70 @@
 import React, { Component } from "react";
-import {postRequest} from '../Utils/RestUtils';
-
+import {getRequest} from '../Utils/RestUtils';
+import './CustomerConsole.css';
 
 class CustomerConsole extends Component{
   state={
     userName:'',
 
   }
-  constructor(props){
-    super(props);
+  constructor(props) {
+    super(props) //since we are extending class Table so we have to use super in order to override Component class constructor
+    this.state = { //state is by default an object
+       students: [
+          { tokenNumber: undefined, assignedCounter: '', isPremium: undefined, ownerName: '' },
+       ]
+    }
+
+
+  }
+
+  refreshConsole(){
+    getRequest('/getallassignedtokens',
+   (data)=>{
+
+    this.setState({students:data})
+     
     
-  }
+   });
+ }
 
-
-  componentDidMount() {
-
-
-    postRequest('/test',{"kedar":"kedar"},
-      (data) =>{
-                console.log("the data is ::::"+data.kedar);
-                this.setState({userName:data.kedar});
-              }
-      
-      );
-
-  }
   render(){
     return (
       <div >
-         <h1>{this.props.name}</h1>
-            id is {this.props.match.params.id}
+            <h1 id='title'>{this.props.name}</h1>
+            <table id='students'>
+               <tbody>
+                  <tr>{this.renderTableHeader()}</tr>
+                  {this.renderTableData()}
+               </tbody>
+            </table>
+          <button onClick={this.refreshConsole.bind(this)}>Refresh</button>
       </div>
     );
   }
 
+  renderTableHeader() {
+    let header = Object.keys(this.state.students[0])
+    return header.map((key, index) => {
+       return <th key={index}>{key.toUpperCase()}</th>
+    })
+ }
+
+
+ renderTableData() {
+  return this.state.students.map((student, index) => {
+     const { tokenNumber, assignedCounter, isPremium, ownerName } = student //destructuring
+     return (
+        <tr key={tokenNumber}>
+           <td>{tokenNumber}</td>
+           <td>{assignedCounter}</td>
+           <td>{isPremium}</td>
+           <td>{ownerName}</td>
+        </tr>
+     )
+  })
+
   
+}
 }
 export default CustomerConsole;
